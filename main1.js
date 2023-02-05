9be9181212d2dc96ed6659c2f09995b5c277f19e
Dadaxon <dm.online1991@gmail.com>
2023-02-05 18:42:14 +0500

    assalom

--- main1.js
+++ main1.js
@@ -52,33 +52,58 @@
 // console.log(Person1);
 //                                                   
                                                   //3-usul
-function adress(davlat,viloyat,shahar){
-    this.davlat=davlat;
-    this.viloyat=viloyat;
-    this.shahar=shahar
-}
-function person(ismi,familiyasi, manzil = adress()){
-    this.ismi = ismi;
-    this.familiyasi=familiyasi;
-    this.manzili=manzil
-}
+// function adress(davlat,viloyat,shahar){
+//     this.davlat=davlat;
+//     this.viloyat=viloyat;
+//     this.shahar=shahar
+// }
+// function person(ismi,familiyasi, manzil = adress()){
+//     this.ismi = ismi;
+//     this.familiyasi=familiyasi;
+//     this.manzili=manzil
+// }
 
-const Person1 =new person("G'anijon","Sattorov",new adress("Uzb","tashkent","Chilonzor"))
-console.log(Person1);
+// const Person1 =new person("G'anijon","Sattorov",new adress("Uzb","tashkent","Chilonzor"))
+// console.log(Person1);
+
+            //2-misol
 ///////////////////////////////////
 // let p3 = {}
 // Object.assign(p3,p1) //obyektdan nusxa olish
-// p3.age = 55
-// console.log(p3);
-// //employed
-// fish:
-// adress:{
-//     country:,
-//     region:,
-//     city
-// }
-// lavozim:
 
-// //talabalar
-// //cars
+
+function talaba_yonalish(bilim_yurti,kanfedra,bosqichi){
+    return {
+        talaba : Object.create({},{
+            bilim_yurti:{
+                value:bilim_yurti,
+                writable:true,
+                enumerable:true,
+                configurable:true
+            },
+            kanfedra:{
+                value:kanfedra,
+                writable:true,
+                enumerable:true,
+                configurable:true
+            },
+            bosqichi:{
+                value:bosqichi,
+                writable:true,
+                enumerable:true,
+                configurable:true
+            },
+        })
+    }
+}
+
+function talaba(ismi,familiyasi,t_yil,talaba_yonalish=talaba_yonalish(bilim_yurti,kanfedra,bosqichi)){
+    this.ismi=ismi,
+    this.familiyasi=familiyasi,
+    this.t_yil=t_yil
+    this.talaba_yonalish=talaba_yonalish
+}
+
+const boburjin =new talaba("Sattor","Valiyev",1991,new talaba_yonalish("tatu","radio",2009))
+console.log(boburjin);
 
