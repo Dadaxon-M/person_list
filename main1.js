da67208daced22b9aba4792e71d0f798973cdbf4
Dadaxon <dm.online1991@gmail.com>
2023-02-08 07:57:27 +0500

    assalom

--- main1.js
+++ main1.js
@@ -50,3 +50,95 @@
 //   }
 // }
 // const Vrach = new Doctor("Islom","Turdiyev",18,"o'g'il","jarroh","mirzo ulugbek");
+
+// class Mashina{
+//     constructor(nomi,modeli,rang,narhi,ot_kuchi,yoqilgi_sarfi){
+//         this.nomi = nomi;
+//         this.modeli = modeli;
+//         this.rang = rang;
+//         this.narhi = narhi;
+//         this.ot_kuchi = ot_kuchi;
+//         this.yoqilgi_sarfi = yoqilgi_sarfi
+//     }
+// }
+// const nexia = new Mashina("Nexia","chevrolet","oq","15000$",105,10)
+// const nexia = new Mashina()
+// console.log(nexia);
+
+
+
+// {key:value}       {  }  //-- lugat, obyekt
+// [manzil,kochcha]    [ 1,2,3]  // --ro'yhat
+
+// const mevalar = ["anjir","olma","banan","behi"]
+// mevalar.unshift("qulupnay")
+// mevalar.push("uzum")
+// mevalar.shift()
+// mevalar.pop()
+// mevalar[mevalar.length] = "anjir"
+// mevalar[100] = "kivi"
+// console.log(mevalar.length);
+// console.log(mevalar);
+// console.log(mevalar);
+
+// const persons = [
+// {
+//     firstname:"sattor",
+//     age:45,
+//     fdsfd:"dfdfdf",
+//     jinsi:"erkak",
+//     adress:{
+//         mamlakat:"O'znekiston",
+//         viloyat:"toshkent",
+//         shahar:"mirzoulug"
+//     }
+// },
+// {
+//     firstname:"Gani",
+//     age:55,
+//     fdsfd:"dfdfdf",
+//     jinsi:"erkak",
+//     adress:{
+//         mamlakat:"O'znekiston",
+//         viloyat:"toshkent",
+//         shahar:"mirzoulug"
+//     }
+// },
+// {
+//     firstname:"farsi",
+//     age:55,
+//     fdsfd:"dfdfdf",
+//     jinsi:"erkak",
+//     adress:{
+//         mamlakat:"O'znekiston",
+//         viloyat:"toshkent",
+//         shahar:"mirzoulug"
+//     }
+// }
+// ]
+
+// console.log(persons[0].firstname);
+
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// const myChildren = myGirls.concat(myBoys);
+// console.log(myChildren);
+
+// const arr1 = ["Emil", "Tobias", "Linus"];
+// const myChildren = arr1.concat("Peter"); 
+// console.log(myChildren);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.splice(0, 3);
+// console.log(fruits.length);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits);
+// fruits.splice(3,0, "Lemon", "Kiwi");
+// console.log(fruits);
+
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(3);
+console.log(citrus);
